5f75501a1d7566ba9f94174b6825d7bd9d302b6a
trickypr <23250792+trickypr@users.noreply.github.com>
2022-07-02 15:58:15 +1000

    Correct type definition

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'is-apple-silicon' {
-  export const isAppleSilicon: boolean
-  export const isNodeNative: boolean
-  export const isRosetta: boolean
+  export const isAppleSilicon: () => boolean
+  export const isNodeNative: () => boolean
+  export const isRosetta: () => boolean
 }
